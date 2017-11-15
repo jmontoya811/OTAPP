38e7ce21158116e7975da226b9f8ef275e5c32ae
jmontoya811 <32622895+jmontoya811@users.noreply.github.com>
2017-11-15 15:02:18 -0700

    implemented edit and update

--- Gemfile
+++ Gemfile
@@ -15,7 +15,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
-
+gem 'factory_girl_rails', '~> 4.9'
 
 group :development, :test do
   gem 'byebug'
@@ -29,6 +29,7 @@ group :development do
   gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,12 +3,10 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe "Creation" do
     before do
-      @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
-       login_as(@user, :scope => :user) 
-      @post = Post.create(date: Date.today, rationale: "Anything")
+     @post = FactoryGirl.create(:post)
     end
 
-   xit 'can be created' do
+   it 'can be created' do
       expect(@post).to be_valid
   end
 
