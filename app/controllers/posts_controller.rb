a21cb174b048e3f1fd8693b74015561b635d02f2
jmontoya811 <32622895+jmontoya811@users.noreply.github.com>
2017-11-15 18:36:11 -0700

    implement destroy method and test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-    before_action :set_post, only: [:show, :edit, :update]
+    before_action :set_post, only: [:show, :edit, :update, :destroy]
     def index
         @posts = Post.all
     end
@@ -28,7 +28,12 @@ class PostsController < ApplicationController
  		else
  			render :edit
 		end
-	end
+    end
+    
+    def destroy
+        @post.delete
+        redirect_to posts_path, notice: 'Your post was deleted successfully'
+    end
 
 
     def show
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "creation" do
-      before do
-     @post = FactoryGirl.create(:user)
+    before do
+       @user = FactoryGirl.create(:user)
     end
 
+  describe "creation" do
    it "can be created" do
       expect(@user).to be_valid
     end
 
-   xit "cannot be created without first_name, last_name" do
+   it "cannot be created without first_name, last_name" do
       @user.first_name = nil
       @user.last_name = nil
       expect(@user).to_not be_valid
@@ -18,8 +18,8 @@ RSpec.describe User, type: :model do
   end
 
    describe "custom name methods" do
-    xit 'has a full name method that combines first and last name' do
-      expect(@user.full_name).to eq("Snow, Jon")
+    it 'has a full name method that combines first and last name' do
+      expect(@user.full_name).to eq("SNOW, JON")
     end
   end 
 end
