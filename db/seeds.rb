0f16814e5c607ca207c8915241e3f4020e858a79
jmontoya811 <32622895+jmontoya811@users.noreply.github.com>
2017-11-21 13:58:35 -0700

    added overtime request

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ puts "AdminTest Created"
 puts "User Test Created"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 Posts have been created"
