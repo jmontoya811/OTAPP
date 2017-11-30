c6abf191c43c69bcf85338202a26764663366183
jmontoya811 <32622895+jmontoya811@users.noreply.github.com>
2017-11-30 15:38:19 -0700

    fixed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,5 +89,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.serve_static_assets = true
-  config.action_mailer.default_url_options = { :host => 'wlp-overtime.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'wlp-overtime-ap.herokuapp.com' }
 end
