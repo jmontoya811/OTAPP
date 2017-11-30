b90cd61d104e84d1c373450a4e936cbb7ef755dc
jmontoya811 <32622895+jmontoya811@users.noreply.github.com>
2017-11-30 15:00:58 -0700

    change seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,15 +19,14 @@ AdminUser.create!(email: "admin@test.com",
 puts "AdminTest Created"
 
 
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+
+puts "3 audit logs have been created"
 
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content I want to see how it looks with other content, so I'm going to add some more data to our seeds.rb file for the post creation process. I'm making the rationale really long to see how the truncate method is behaving.", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 Posts have been created"
-
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
-
-puts "3 audit logs have been created"
