4bc0360d025a7996a270c67494c4ee06d9a2582e
jmontoya811 <32622895+jmontoya811@users.noreply.github.com>
2017-11-30 16:07:57 -0700

    rake task for SMSTool

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-@user = User.create!(email: "test@test.com",
+@employee = Employee.create!(email: "test@test.com",
                                   password: "asdfasdf", 
                                   password_confirmation: "asdfasdf",
                                   first_name: "Jon", 
@@ -6,7 +6,7 @@
                                   phone: "3854049924")
 
 
-puts "User Test Created"
+puts "Employee Created"
 
 AdminUser.create!(email: "admin@test.com",
                                 password: "asdfasdf",
@@ -19,14 +19,14 @@ AdminUser.create!(email: "admin@test.com",
 puts "AdminTest Created"
 
 
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 20.days))
 
 puts "3 audit logs have been created"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content I want to see how it looks with other content, so I'm going to add some more data to our seeds.rb file for the post creation process. I'm making the rationale really long to see how the truncate method is behaving.", user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content I want to see how it looks with other content, so I'm going to add some more data to our seeds.rb file for the post creation process. I'm making the rationale really long to see how the truncate method is behaving.", user_id: @employee.id, overtime_request: 2.5)
 end
 
 puts "100 Posts have been created"
--- lib/tasks/notification.rake
+++ lib/tasks/notification.rake
@@ -1,19 +1,14 @@
 namespace :notification do
   desc "send SMS notification to employees requesting them to log OT"
   task sms: :environment do
-    # 1. Schedule to run at Sunday at 5pm
-    # 2. Iterate over all employees
-    # 3. Skip AdminUsers
-    # 4. Send a message that has instructions and a link to log time
-    # User.all.each do |user|
-    #   SmsTool.send_sms()
-    # end
-    # number: "555-555-3323"
-    # number: "5555553323"
-    # No spaces or dashes
-    # exactly 10 characters
-    # all characters have to be a number
+    if Time.now.sunday?
+      employees = Employee.all
+      notification_message = "Please log into the overtime management dashhboard to request overtime or confirm your hours for last week: https://wlp-overtime-ap.herokuapp.com"
+
+      employees.each do |employee|
+        SmsTool.send_sms(number: employee.phone, message: notification_message)
     end
+  end
 
     desc "Sends mail notification to managers (admin users) each day to inform of pending overtime requests"
       task manager_email: :environment do
